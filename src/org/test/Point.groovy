fddcdb6d7feb02b148976db148ee00d42f3cabaa
Gary Dinh <garydinh@Garys-MacBook-Pro.local>
2016-12-06 11:32:31 +0800

    add some methods

--- src/org/test/Point.groovy
+++ src/org/test/Point.groovy
@@ -2,4 +2,12 @@ package org.test
 
 class Point {
     float x, y, z;
+    Point(float x, float y, float z){
+	this.x = x;
+	this.y = y;
+	this.z = z;
+    }
+    public String toString(){
+	return "string";
+    }
 }
